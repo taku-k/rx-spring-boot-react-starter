8991eaad182e11484c0c3698f069f86a8bdd3615
Takuya Kuwahara <taakuu19@gmail.com>
2016-11-20 22:07:50 +0900

    add TodoService test

---
+++ server/src/test/java/server/services/TodoServiceTest.java
@@ -0,0 +1,40 @@
+package server.services;
+
+import org.junit.Test;
+import rx.observers.TestSubscriber;
+import server.domain.Todo;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+public class TodoServiceTest {
+    @Test
+    public void testGetEmptyTodoList() {
+        TodoService todoService = new TodoService();
+        TestSubscriber<List<Todo>> testSubscriber = new TestSubscriber<>();
+        todoService.getTodoList().subscribe(testSubscriber);
+
+        // No emit error
+        testSubscriber.assertNoErrors();
+        List<Todo> emptyTodo = testSubscriber.getOnNextEvents().get(0);
+        assertThat(emptyTodo)
+                .as("Get empty todo list")
+                .isEmpty();
+    }
+
+    @Test
+    public void testOneTodoList() {
+        TodoService todoService = new TodoService();
+        // Add one text to the list
+        todoService.addTodo("todo1").toBlocking().first();
+        TestSubscriber<List<Todo>> testSubscriber = new TestSubscriber<>();
+        todoService.getTodoList().subscribe(testSubscriber);
+
+        testSubscriber.assertNoErrors();
+        List<Todo> oneTodo = testSubscriber.getOnNextEvents().get(0);
+        assertThat(oneTodo)
+                .as("List contains one message `todo1`")
+                .extracting(Todo::getText)
+                .containsExactly("todo1");
+    }
+}
