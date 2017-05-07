c3adb63db49892ec7995b5cd414950c754ee83ad
Takuya Kuwahara <taakuu19@gmail.com>
2017-05-07 20:39:47 +0900

    Use Cache of guave instead of ConcurrentMap

--- server/src/main/java/server/services/TodoServiceImpl.java
+++ server/src/main/java/server/services/TodoServiceImpl.java
@@ -1,20 +1,25 @@
 package server.services;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import server.domain.Todo;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class TodoServiceImpl implements TodoService {
-    private final ConcurrentHashMap<Long, Todo> todoById = new ConcurrentHashMap<>();
+    private final Cache<Long, Todo> todoById = CacheBuilder.newBuilder()
+                                                        .maximumSize(100)
+                                                        .expireAfterWrite(10, TimeUnit.MINUTES)
+                                                        .build();
     private final AtomicLong lastId = new AtomicLong(0);
 
     public Flowable<List<Todo>> getTodoList() {
-        return Flowable.just(Arrays.asList(todoById.values().toArray(new Todo[]{})));
+        return Flowable.just(Arrays.asList(todoById.asMap().values().toArray(new Todo[]{})));
     }
 
     public Flowable<Todo> addTodo(String text) {
@@ -29,8 +34,8 @@ public class TodoServiceImpl implements TodoService {
 
     public Flowable<Todo> deleteTodoById(Long todoId) {
         return Flowable.create(emitter -> {
-            Todo delTodo = todoById.get(todoId);
-            todoById.remove(todoId);
+            Todo delTodo = todoById.getIfPresent(todoId);
+            todoById.invalidate(todoId);
             emitter.onNext(delTodo);
             emitter.onComplete();
         }, BackpressureStrategy.BUFFER);
